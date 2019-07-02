d22681e6e4910d112c886e388a0057f2107074d4
Kelsey Cox <kelseycox@Lucy.local>
2019-07-02 16:19:15 -0700

    search by lat and lng

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const db = require('../models')
 
 const router = express.Router();
 
 //GET / - show your name, click to go to lists for visited/want to visit
+router.get('/', function (req, res) {
+    res.render('profile/index')
+})
 
 //GET /list - show list of stores user wants to go to
+router.get('/list', function (req, res) {
+    db.user.findByPk(req.user.id).then(function (user) {
+        user.getLocations().then(function (locations) {
+            console.log(locations)
+            res.render('profile/list', { locations })
+        })
+    })
+    // res.render('profile/list')
+})
 
 //GET /visited - show list of stores user has gone to, including notes
 
--- controllers/stores.js
+++ controllers/stores.js
@@ -3,8 +3,13 @@ const express = require('express');
 const axios = require('axios');
 const db = require('../models')
 
-
 const router = express.Router();
+
+//geocoding setup
+const mapbox = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mapbox({
+    accessToken: process.env.MAPBOX_PUBLIC_KEY
+})
 
 //GET / - show search page
 router.get('/', function (req, res) {
@@ -14,17 +19,23 @@ router.get('/', function (req, res) {
 //GET - /search show 
 router.get('/search', function (req, res) {
     let location = req.query.city + ',' + req.query.state
-    let apiUrl = 'https://api.ravelry.com/shops/search.json?shop_type_id=1&query=' + location;
-    let headers = {
-        'Authorization': 'Basic ' + Buffer.from(`${process.env.APIUSER}:${process.env.APIPASS}`).toString('base64')
-    };
+    geocodingClient.forwardGeocode({
+        query: location
+    }).send().then(function (response) {
+        let results = response.body.features[0].center
+        console.log(results)
 
-    axios.get(apiUrl, {headers} ).then( function(response) {
-        let shopData = response.data
-        console.log(shopData)
-        res.render('stores/map', { shopData })
-    }).catch(function(error) {
-        res.json(error);
+        let headers = {
+            'Authorization': 'Basic ' + Buffer.from(`${process.env.APIUSER}:${process.env.APIPASS}`).toString('base64')
+        };
+        
+        let apiUrl = 'https://api.ravelry.com/shops/search.json?shop_type_id=1&radius=20&units=miles&lat=' + results[1] + '&lng=' + results[0];
+        axios.get(apiUrl, {headers} ).then( function(response) {
+            let shopData = response.data
+            res.render('stores/map', { shopData })
+        }).catch(function(error) {
+            res.json(error);
+        });
     });
 });
 
@@ -45,20 +56,32 @@ router.get('/search/:id', function (req, res) {
 
 //POST /search - add to go-to list
 router.post('/add/:id', function (req, res) {
-    db.location.findOrCreate({
-        where: {
-            name: req.body.name,
-            city: req.body.city,
-            state: req.body.state,
-            ravId: req.body.ravId
-        }
-    }).spread(function (shop, created) {
-        console.log(shop)
-        res.send('something got created!')
-        // use session data to go back to the previous page?
-    }).catch(function (error) {
-        res.send('something went wrong and/or it already existed');
-    })  
+    // find the user
+    db.user.findByPk(req.user.id).then(function (user) {
+        db.location.findOrCreate({
+            where: {
+                name: req.body.name,
+                city: req.body.city,
+                state: req.body.state,
+                ravId: req.body.ravId
+            }
+        }).spread(function (store, created) {
+            db.usersLocations.findOrCreate({
+                where: {
+                    userId: req.user.id,
+                    locationId: store.id,
+                    visited: false
+                }
+            }).then(function (data) {
+            // user.addLocation(shop).then(function (data){
+                console.log(shop)
+                // redirect me!
+                res.send('we did something!')
+            });
+        }).catch(function (error) {
+            res.send(error);
+        });  
+    });
 });
 
 module.exports = router;
